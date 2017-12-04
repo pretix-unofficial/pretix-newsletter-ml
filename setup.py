5670c7c37018221975b6a2b28b0daf252c8bbf8b
Raphael Michel <mail@raphaelmichel.de>
2017-12-04 21:03:13 +0100

    Bump release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ cmdclass = {
 
 setup(
     name='pretix-newsletter-ml',
-    version='1.0.0',
+    version='1.1.0',
     description='pretix newsletter integration for mailing lists',
     long_description=long_description,
     url='https://github.com/pretix/pretix-newsletter-ml',
