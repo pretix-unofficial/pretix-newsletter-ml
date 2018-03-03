b13fa0392dc837b1affacdb187290ee6e01e28e3
Raphael Michel <mail@raphaelmichel.de>
2018-03-03 15:25:30 +0100

    Bump version

--- pretix_newsletter_ml/__init__.py
+++ pretix_newsletter_ml/__init__.py
@@ -10,7 +10,7 @@ class PluginApp(AppConfig):
         author = 'Raphael Michel'
         description = 'pretix newsletter integration for mailing lists'
         visible = True
-        version = '1.1.0'
+        version = '1.2.0'
 
     def ready(self):
         from . import signals  # NOQA
